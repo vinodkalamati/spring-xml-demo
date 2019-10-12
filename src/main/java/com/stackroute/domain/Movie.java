cfefbd0264c6fd679fed0910a0c2a423c6275d37
vinodkalamati <54061209+vinodkalamati@users.noreply.github.com>
2019-10-12 11:27:21 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -9,7 +9,7 @@ public class Movie {
         this.actor = actor;
     }
 
-    public void getdetails() {
+    public void getActorDetails() {
         System.out.println(
                 " actorName= " + actor.getName() +" age= "+actor.getAge()+" gender= "+actor.getGender());
     }
